30bb708454d11deb2e5467b0737d4422c028f7ed
Jason Aguirre <jason_aguirre@uri.edu>
2022-12-15 20:17:15 -0500

    Compelete rum, non-optimized

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,10 @@ mod instruct;
 mod init;
 
 fn main() {
-    env::set_var("RUST_BACKTRACE", "0");
     let input = env::args().nth(1);
     let mut machine = init::initialize(input.clone());
     while machine.program_counter < machine.memory[0].len() as u32 {
         let instruction = machine.memory[0][machine.program_counter as usize];
-        //println!("{}", instruction);
         instruct::perform(instruction, &mut machine)
     }
 }
